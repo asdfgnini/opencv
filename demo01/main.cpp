8aa85d1270bea7fa4b22a69d94949a2325a24ba3
zjt <2223143694@qq.com>
2023-11-29 11:01:47 +0800

    添加opencv库，版本4.8

--- demo01/main.cpp
+++ demo01/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    //输出opencv的版本号信息
+    cout << CV_VERSION <<endl;
+
+
+
     return 0;
 }
